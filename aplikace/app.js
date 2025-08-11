0a76321f88f3f1a4d5ef2b26aa3f8a1f9df81bd9
martinecvia <martin.coplak@viapont.cz>
2025-08-11 22:59:18 +0200

    V2, Added more things to app

--- aplikace/app.js
+++ aplikace/app.js
@@ -3,13 +3,9 @@ var app = angular.module('calculator', ["ui.bootstrap"]);
 app.controller("form", ["$scope", "$http", function($scope, $http) {
 
     $scope.Math = window.Math;
+    $scope.viewObce = obce || [];
 
-    $scope.okresy = Object.keys(okresy);
-    $scope.obce   = Object.keys(obce);
     $scope.stavby = Object.keys(stavby);
-
-    $scope.viewOkresy = $scope.okresy;
-    $scope.viewObce = $scope.obce;
 
     // Fetch the counter data
     $http.get("https://api.warfaremc.eu/v1/apko.cz/counter").then(response => {
@@ -18,51 +14,39 @@ app.controller("form", ["$scope", "$http", function($scope, $http) {
         }
     });
 
-    $scope.$watch("okres", function(o, n) {
-        if (o) {
-            // Skontroluj jestli je okres validní nebo ne
-            if ($scope.okresy.includes(o)) {
-                // Kontrola jestli je zadána obec a kontrola jestli se obec nachází v okresu
-                if ($scope.obec && !okresy[o].includes($scope.obec)) {
-                    console.log("Obec se nenachází v zadaném okresu")
-                    $scope.obec = null
-                    $scope.viewObce = $scope.okresy[o] || $scope.obce;
-                }
-                $scope.okres = o
-                $scope.viewObce = $scope.okresy[o] || $scope.obce;
-                return
-            }
-            $scope.okres = null
-        }
-        $scope.viewOkresy = $scope.okresy;
-    });
+    $scope.showDropdown = false;
+    $scope.selectedTown = "";
 
-    $scope.$watch("obec", function(o, n) {
-        if (o) {
-            // Skontroluj jestli je obec validní nebo ne
-            if ($scope.obce.includes(o)) {
-                // Kontrola jestli je zadán okres a kontrola jestli zadaná obec patří do okresu
-                if ($scope.okres && !obce[o].includes($scope.okres)) {
-                    console.log("Okres nepatří k zadané obci")
-                    $scope.okres = null
-                    $scope.viewOkresy = $scope.obce[o] || $scope.okresy;
-                }
-                $scope.obec = o
-                $scope.viewOkresy = $scope.obce[o] || $scope.okresy;
-                return
-            }
-            $scope.obec = null
-        }
-        $scope.viewObce = $scope.obce;
-    });
+    $scope.selectTown = function(town) {
+        $scope.selectedTown = town;
+        $scope.searchText = town;
+        $scope.showDropdown = false;
+    };
 
-    $scope.onFocus = function(e) {
-        $(e.target).trigger("input").trigger("change");
+    $scope.hideDropdown = function() {
+        // Malé zpoždění, aby šel klik zachytit dřív než zmizí
+        setTimeout(function() {
+            $scope.$apply(function() {
+                $scope.showDropdown = false;
+            });
+        }, 200);
+    };
+
+    $scope.getRootByType = function(druhStavby) {
+        let posledniSkupina = null;
+        for (const key in stavby) {
+            if (stavby[key] === null) {
+                posledniSkupina = key;
+            } else if (key === druhStavby) {
+                return posledniSkupina;
+            }
+        }
+        return null;
     };
 
     $scope.recalculate = function() {
         if (!$scope.stavbyList) {
-            $scope.stavbyList = [{}]; // Vloží prázdnou stavbu pokud je stránka načtena prvně
+            $scope.stavbyList = [{}];
         }
         for (stavba in $scope.stavbyList) {
             stavba = $scope.stavbyList[stavba];
@@ -71,26 +55,29 @@ app.controller("form", ["$scope", "$http", function($scope, $http) {
                     stavba.jednotky = Object.values(stavby[stavba.druhStavby]);
                     if (!stavba.poctyJednotek) {
                         stavba.poctyJednotek = {};
-                        stavba.kr = 0.0
-                        stavba.dl = 0.0
                     }
+                    stavba.kr = 0.0;
+                    stavba.dl = 0.0;
                     for (j in stavba.jednotky) {
                         var jednotka = stavba.jednotky[j];
-                        if (stavba.poctyJednotek[jednotka]) {
-                            const factor = Math.pow(10, 2)
-                            koeficient = stavba.koeficient / 100 || 1 // Redukční koeficient
-                            pocet = stavba.poctyJednotek[jednotka]
-                            stavba.kr = Math.round(((pocet / jednotka.metric) * jednotka.kr * koeficient) * factor) / factor // Dlouhodobé stáni
-                            stavba.dl = Math.round(((pocet / jednotka.metric) * jednotka.dl * koeficient) * factor) / factor // Krátkodobé stání
+                        if (stavba.poctyJednotek[jednotka.uuid]) {  
+                            koeficient = stavba.koeficient / 100 || 1;
+                            var pocet = stavba.poctyJednotek[jednotka.uuid];
+                            stavba.kr += ((pocet / jednotka.metric) * jednotka.kr * koeficient);
+                            stavba.dl += ((pocet / jednotka.metric) * jednotka.dl * koeficient);
                         }
                     }
+                    const factor = Math.pow(10, 2);
+                    stavba.kr = Math.round(stavba.kr * factor) / factor;
+                    stavba.dl = Math.round(stavba.dl * factor) / factor;
                 } else {
-                    stavba.druhStavby = ""; stavba.kr = 0.0; stavba.dl = 0.0; stavba.poctyJednotek = {}; stavba.koeficient = null // Pokud je vybrána skupina tak vyčisti počítadlo
+                    stavba.druhStavby = ""; stavba.kr = 0.0; stavba.dl = 0.0; stavba.poctyJednotek = {}; stavba.koeficient = null
                 }
             }
         }
         $scope.kr = Math.round($scope.stavbyList.filter((o) => o.druhStavby != "").reduce((sum, stavba) => sum + (stavba.kr || 0.0), 0))
         $scope.dl = Math.round($scope.stavbyList.filter((o) => o.druhStavby != "").reduce((sum, stavba) => sum + (stavba.dl || 0.0), 0))
+        $scope.celkem = Math.round(($scope.kr || 0) + ($scope.dl || 0));
     };
 
     $scope.$watch("stavbyList", $scope.recalculate, true);
